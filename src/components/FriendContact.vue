110f2d6938add3c5d539c9654e1e1154e0ac2e71
Chris.Vasquez <cvasqu09@gmail.com>
2020-12-20 18:18:12 -0500

    Adding friend contact component

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,56 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">Show details</button>
+    <button @click="toggleFavorite">Toggle favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'FriendContact',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: false,
+      default: 'test@email.com',
+    },
+  },
+  emits: {
+    'toggle-favorite': (id) => !!id,
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>

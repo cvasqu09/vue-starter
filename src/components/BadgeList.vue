4e0a91b586268ce36ac60839974b96fe78a9a57f
Chris.Vasquez <cvasqu09@gmail.com>
2020-12-21 21:35:36 -0500

    Deep dive initial push

---
+++ src/components/BadgeList.vue
@@ -0,0 +1,41 @@
+<template>
+  <BaseCard>
+    <template v-slot:header>
+      <h2>Available Badges</h2>
+    </template>
+    <template v-slot:content>
+      <ul>
+        <li>
+          <base-badge type="admin" caption="ADMIN"></base-badge>
+        </li>
+        <li>
+          <base-badge type="author" caption="AUTHOR"></base-badge>
+        </li>
+      </ul>
+    </template>
+  </BaseCard>
+</template>
+
+<script>
+import BaseCard from "@/components/BaseCard";
+export default {
+  components: {
+    BaseCard
+  }
+}
+</script>
+
+<style scoped>
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+}
+
+li {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseCard>
+    <template v-slot:header>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </template>
+    <template v-slot:content>
+      <p>{{ infoText }}</p>
+    </template>
+  </BaseCard>
+</template>
+
+<script>
+import BaseCard from "@/components/BaseCard";
+import BaseBadge from "@/components/BaseBadge";
+export default {
+  components: {BaseCard, BaseBadge},
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>

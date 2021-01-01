65ecf003b1f58e4be9ebdcc89ffbfe5c31c782a7
Chris.Vasquez <cvasqu09@gmail.com>
2020-12-31 20:46:16 -0500

    Initial assignment task

--- src/App.vue
+++ src/App.vue
@@ -1,115 +1,29 @@
 <template>
-  <section>
-    <header><h1>My Friends</h1></header>
-    <ul>
-      <friend-contact
-        v-for="friend in friends"
-        :key="friend.id"
-        :name="friend.name"
-        :id="friend.id"
-        @toggle-favorite="toggleFavorite"
-        :email-address="friend.email"
-        :phone-number="friend.phone">
-      </friend-contact>
-    </ul>
-  </section>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3>OUTPUT COURSE GOAL</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button>Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
 </template>
 
 <script>
-import FriendContact from '@/components/FriendContact.vue';
-
 export default {
-  components: { FriendContact },
-  data() {
-    return {
-      friends: [
-        {
-          id: 'manuel',
-          name: 'Manuel',
-          email: 'test',
-          phone: 'test',
-        },
-        {
-          id: 'karen',
-          name: 'karen',
-          phone: 'test',
-        },
-      ],
-    };
-  },
-  methods: {
-    toggleFavorite(id) {
-      alert(`here${id}`);
-    },
-  },
-};
+  
+}
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
-
-* {
-  box-sizing: border-box;
-}
-
 html {
-  font-family: 'Jost', sans-serif;
+  font-family: sans-serif;
 }
-
 body {
-  margin: 0;
-}
-
-header {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #58004d;
-  color: white;
+  margin: 3rem;
   text-align: center;
-  width: 90%;
-  max-width: 40rem;
 }
-
-#app ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-#app li {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-
-#app h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #58004d;
-  margin: 0 0 1rem 0;
-}
-
-#app button {
-  font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
-  color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-}
-
-#app button:hover,
-#app button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
-}
-
 </style>
